065bb0ea6907eaa25edd56d7601a9e98c962e551
mashroofmashru <mashroofvlk@gmail.com>
2025-05-16 22:44:36 +0530

    UI designed, List Todos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todo,setTodo]=useState([]);
+  const [todos,setTodos]=useState('');
+  console.log(todo);
+  console.log(todos)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's {new Date().toLocaleDateString('en-US', { weekday: 'long' })} 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input type="text" onChange={(e)=>setTodos(e.target.value)} placeholder="🖊️ Add item..." value={todos}/>
+        <i className="fas fa-plus" onClick={()=>setTodo([...todo,{id:Date(),text:todos,status:false}])}></i>
+      </div>
+      <div className="todos">
+        {todo.map((obj)=>{
+          return(
+          <div className="todo" key={obj.id}>
+          <div className="left">
+            <input type="checkbox" name="" id={obj.id} onChange={(e)=>{
+              todo.filter((obj2)=>{
+                if(obj2.id===obj.id){obj.status=e.target.checked}
+                return obj2
+              })
+            }
+            }/>
+            <p>{obj.text}</p>
+          </div>
+          <div className="right">
+            <i className="fas fa-times"></i>
+          </div>
+        </div>)
+      })}
+      </div>
     </div>
   );
 }
